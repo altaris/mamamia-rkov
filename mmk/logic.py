87c5676a63c4cedf0db26c5ec55c1c1f78d5373f
Cédric HT <nope-mcnopeface@asdf.org>
2021-07-07 16:14:12 +0900

    Added within clauses

--- mmk/logic.py
+++ mmk/logic.py
@@ -14,26 +14,34 @@ from .markov_chain import MarkovChain
 grammar = Grammar(
     r"""
     formula             = always_formula /
+                          eventually_within_formula /
                           eventually_formula /
                           not_formula /
+                          until_within_formula /
                           until_formula /
                           state_list
 
     always_formula      = always ws formula
-    eventually_formula  = diamond ws formula
+    eventually_formula  = eventually ws formula
+    eventually_within_formula
+                        = eventually ws within ws number ws formula
     not_formula         = not ws formula
     until_formula       = state_list ws until ws formula
+    until_within_formula
+                        = state_list ws until ws within ws number ws formula
 
-    always              = "always" / "henceforth" / "[]"
-    diamond             = "eventually" / "<>"
-    not                 = "not" / "¬"
-    until               = "until" / "U"
+    always          = "always" / "henceforth" / "[]"
+    eventually      = "eventually" / "<>"
+    not             = "not" / "¬"
+    until           = "until" / "U"
+    within          = "within"
 
-    state               = ~"[A-Z0-9_]+"i
-    comma_state         = "," state
-    state_list          = state comma_state*
+    state           = ~"[A-Z0-9_]+"i
+    comma_state     = "," state
+    state_list      = state comma_state*
 
-    ws                  = ~"\s+"
+    number          = ~"[0-9]+"i
+    ws              = ~"\s+"
     """
 )
 
@@ -52,17 +60,35 @@ class FormulaVisitor(NodeVisitor):
     def visit_eventually_formula(self, node, visited_children):
         return {"child": visited_children[-1], "type": "eventually"}
 
+    def visit_eventually_within_formula(self, node, visited_children):
+        return {
+            "child": visited_children[-1],
+            "type": "eventually",
+            "within": visited_children[4],
+        }
+
     def visit_formula(self, node, visited_children):
         return visited_children[0]
 
     def visit_not_formula(self, node, visited_children):
         return {"child": visited_children[-1], "type": "not"}
 
+    def visit_number(self, node, visited_children):
+        return int(node.text)
+
     def visit_until_formula(self, node, visited_children):
         return {
             "left_child": visited_children[0],
             "right_child": visited_children[-1],
             "type": "until",
+        }
+
+    def visit_until_within_formula(self, node, visited_children):
+        return {
+            "left_child": visited_children[0],
+            "right_child": visited_children[-1],
+            "type": "until",
+            "within": visited_children[6],
         }
 
     def visit_state(self, node, visited_children):
@@ -124,15 +150,22 @@ def probability_of_formula(
         s = [a for i, a in enumerate(chain._states) if p[i] > 0.0]
         y = chain.state_mask(s)
         s = chain.state_complement(s, chain.predecessors(s))
-        a = chain.restrict_tensor_to_states(
-            np.identity(n) - chain.probability_matrix(), s
-        )
         b = chain.probability_matrix() @ p
-        if np.linalg.matrix_rank(a) == n:
-            x = np.linalg.solve(a, b)
+        if "within" in formula:
+            a = chain.restrict_tensor_to_states(chain.probability_matrix(), s)
+            x = np.zeros(n)
+            for _ in range(formula["within"]):
+                x = a @ x + b
         else:
-            x, *_ = np.linalg.lstsq(a, b, rcond=None)
-        return x + y
+            a = chain.restrict_tensor_to_states(
+                np.identity(n) - chain.probability_matrix(), s
+            )
+            if np.linalg.matrix_rank(a) == n:
+                x = np.linalg.solve(a, b)
+            else:
+                x, *_ = np.linalg.lstsq(a, b, rcond=None)
+            x += y
+        return x
 
     if formula["type"] == "not":
         return 1.0 - probability_of_formula(chain, formula["child"])
@@ -146,7 +179,13 @@ def probability_of_formula(
         s = chain.state_complement(s0 + s1)
         a = chain.restrict_tensor_to_states(chain.probability_matrix(), s)
         b = chain.probability_matrix() @ p_right
-        return fixed_point(lambda x: a @ x + b, np.zeros(len(chain._states)))
+        x = np.zeros(len(chain._states))
+        if "within" in formula:
+            for _ in range(formula["within"]):
+                x = a @ x + b
+        else:
+            x = fixed_point(lambda y: a @ y + b, x)
+        return x
 
     if formula["type"] == "states":
         return np.array([float(s in formula["states"]) for s in chain._states])
